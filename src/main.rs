9df32ee01f165a060885740d16f5f127a170e018
Szymon Kubica <szymo.kubica@gmail.com>
2023-12-20 23:49:51 +0100

    Added the simple benchmark of the checksum algorithm against the native implememtation. VM gives roughly 10x slowdown.

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,47 @@
-use esp_idf_svc::{
-    hal::{gpio::AnyIOPin, peripherals::Peripherals, units::Hertz},
-    sys::sleep,
-};
-use rbpf::{EbpfVmMbuff, EbpfVmNoData};
-use std::fmt::Write;
-
 extern crate rbpf;
 
-// Task 1: support loading arbitary ebpf code that doesn't take any packet
-// data as input.
+use std::path::PathBuf;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
-fn test_ebpf_vm_no_data() {
-    // This is the eBPF program, in the form of bytecode instructions.
-    let prog = &[
-        0xb4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov32 r0, 0
-        0xb4, 0x01, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, // mov32 r1, 2
-        0x04, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, // add32 r0, 1
-        0x0c, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // add32 r0, r1
-        0x95, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // exit
-    ];
-
-    // Instantiate a struct EbpfVmNoData. This is an eBPF VM for programs that
-    // takes no packet data in argument.
-    // The eBPF program is passed to the constructor.
-    let vm = EbpfVmNoData::new(Some(prog)).unwrap();
-
-    // Execute (interpret) the program. No argument required for this VM.
-    let result = vm.execute_program().unwrap();
-    assert_eq!(result, 0x3);
-    log::info!("{}", result)
+fn load_fletcher32_program() -> Vec<u8> {
+    // Hardcoded program bytes because I can't figure out a way to easily load
+    // the ebpf byte code from a file. Possible options would be to host a http
+    // server and accept the bytes in a post request. or set up a file system
+    // on the microcontroller (e.g. SPIFFS or littlefs).
+    [183, 0, 0, 0, 46, 5, 135, 46, 149, 0, 0, 0, 0, 0, 0, 0].to_vec()
 }
 
-fn test_ebpf_vm_buff() {
-    let prog = &[
-        // Load mem from mbuff at offset 8 into R1
-        0x79, 0x11, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, // ldhx r1[2], r0
-        0x69, 0x10, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x95, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00,
-    ];
-    let mem = &mut [0xaa, 0xbb, 0x11, 0x22, 0xcc, 0xdd];
+fn fletcher32_native() -> u32 {
+    let message = r#"This is a test message for the Fletcher32 checksum algorithm.\n"#;
 
-    // Just for the example we create our metadata buffer from scratch, and
-    // we store the pointers to packet data start and end in it.
-    let mut mbuff = &mut [0u8; 32];
-    unsafe {
-        let mut data = mbuff.as_ptr().offset(8) as *mut u64;
-        let mut data_end = mbuff.as_ptr().offset(24) as *mut u64;
-        *data = mem.as_ptr() as u64;
-        *data_end = mem.as_ptr() as u64 + mem.len() as u64;
+    let mut length = message.len() / 2;
+
+    let mut c0: u32 = 0;
+    let mut c1: u32 = 0;
+
+    // Checksum magic
+    while length > 0 {
+        let mut blocklen: usize = length;
+        if blocklen > 360 * 2 {
+            blocklen = 360 * 2;
+        }
+        length -= blocklen;
+        for i in (0..blocklen).step_by(2) {
+            let c;
+            unsafe {
+                c = *message.as_bytes().get_unchecked(i);
+            }
+            c0 = c0 + c as u32;
+            c1 = c1 + c0;
+        }
+        c0 = c0 % 65535;
+        c1 = c1 % 65535;
     }
 
-    // This eBPF VM is for program that use a metadata buffer.
-    let mut vm = EbpfVmMbuff::new(Some(prog)).unwrap();
-    let result = vm.execute_program(mem, mbuff).unwrap();
-    assert_eq!(result, 0x2211);
-    log::info!("{}", result)
+    let checksum = c1 << 16 | c0;
+
+    return checksum;
 }
 
 fn main() {
@@ -65,31 +52,62 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take().unwrap();
-    let pins = peripherals.pins;
+    let prog = load_fletcher32_program();
 
-    let config = esp_idf_svc::hal::uart::config::Config::default().baudrate(Hertz(115_200));
+    let vm = rbpf::EbpfVmNoData::new(Some(&prog)).unwrap();
 
-    let mut uart: esp_idf_svc::hal::uart::UartDriver = esp_idf_svc::hal::uart::UartDriver::new(
-        peripherals.uart1,
-        pins.gpio1,
-        pins.gpio3,
-        Option::<AnyIOPin>::None,
-        Option::<AnyIOPin>::None,
-        &config,
-    )
-    .unwrap();
+    let now = Instant::now();
+    let res = vm.execute_program().unwrap();
+    let elapsed = now.elapsed();
+    log::info!("Program returned: {:?} ({:#x})", res, res);
+    log::info!("Elapsed: {:.2?}", elapsed);
 
+    let now = Instant::now();
+    let checksum = fletcher32_native();
+    let elapsed = now.elapsed();
+    log::info!(
+        "Natively calculated checksum: {:?} ({:#x})",
+        checksum,
+        checksum
+    );
+    log::info!("Elapsed: {:.2?}", elapsed);
+
+    let iterations = 1000;
     loop {
-        for i in 0..10 {
-            writeln!(uart, "{:}", format!("count {:}", i)).unwrap();
+        let mut vm_times: Vec<Duration> = Vec::new();
+        let mut native_times: Vec<Duration> = Vec::new();
+
+        for _ in 0..iterations {
+            let (vm_time, native_time) = benchmark_iteration(&prog);
+            //log::info!("Native: {:.2?}, VM: {:.2?}", native_time, vm_time);
+            vm_times.push(vm_time);
+            native_times.push(native_time);
         }
 
-        log::info!("Hello, world!");
-        test_ebpf_vm_no_data();
-        test_ebpf_vm_buff();
-        unsafe {
-            sleep(1000);
-        }
+        let vm_avg = vm_times.iter().sum::<Duration>() / iterations;
+        let native_avg = native_times.iter().sum::<Duration>() / iterations;
+
+        log::info!(
+            "Average native: {:.2?}, Average VM: {:.2?}",
+            native_avg,
+            vm_avg
+        );
+        sleep(Duration::from_millis(10000));
     }
+}
+
+fn benchmark_iteration(prog: &Vec<u8>) -> (Duration, Duration) {
+    let now = Instant::now();
+    // Decided to move the vm initialisation here as it should be
+    // classified as a part of the execution time.
+    let vm = rbpf::EbpfVmNoData::new(Some(prog)).unwrap();
+    let res = vm.execute_program().unwrap();
+    let elapsed1 = now.elapsed();
+
+    let now = Instant::now();
+    let checksum = fletcher32_native();
+    let elapsed2 = now.elapsed();
+
+    assert_eq!(res, checksum.into(), "Checksum values don't match");
+    return (elapsed1, elapsed2);
 }
